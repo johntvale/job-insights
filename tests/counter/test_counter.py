60ffd1c048d550f2f702dbcb4854aed8e65eac78
johntvale <johntvale@gmail.com>
2022-06-08 11:07:24 -0400

    Requisito 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,18 @@
-# from src.counter import count_ocurrences
+import pytest
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    VALID_PY = 1639
+    VALID_JS = 122
+
+    assert count_ocurrences('src/jobs.csv', 'Python') == VALID_PY
+    assert count_ocurrences('src/jobs.csv', 'Javascript') == VALID_JS
+
+    with pytest.raises(TypeError):
+        count_ocurrences('src/jobs.csv')
+        count_ocurrences()
+
+    with pytest.raises(AssertionError):
+        assert count_ocurrences('src/jobs.csv', 'Python') != VALID_PY
+        assert count_ocurrences('src/jobs.csv', 'Javascript') != VALID_JS
